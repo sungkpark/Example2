337ee5ecb60dc5e9e28e09c0265d269b3c5edd37
Sungkyung Park <SKP@wlan-145-94-179-241.wlan.tudelft.nl>
2018-11-19 15:27:44 +0100

    button working

---
+++ app.js
@@ -0,0 +1,11 @@
+var main = function () { 
+    "use strict";
+
+    $(".comment-input button").on("click", function (event) {
+        var $new_comment = $("<p>").text($(".comment-input input").val());
+
+        $(".comments").append($new_comment);
+    }); 
+};
+    
+$(document).ready(main);
